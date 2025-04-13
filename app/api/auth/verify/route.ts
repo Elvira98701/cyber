996fb15b98e6c5af5d48804814dfbfd9bec05be5
Elvira Latypova <ellalatypova666@gmail.com>
2025-04-13 05:11:43 +0500

    fix

--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -1,9 +1,11 @@
 import { prisma } from "@/prisma/prisma-client";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest) {
   try {
-    const code = "";
+    const code = req.nextUrl.searchParams.get("code");
 
     if (!code) {
       return NextResponse.json({ error: "Invalid code" }, { status: 400 });
